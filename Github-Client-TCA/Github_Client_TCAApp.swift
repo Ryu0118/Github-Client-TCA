e687526fa1736cdf0c59b5d23f998270a6ef3057
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2022-12-16 17:48:59 +0900

    Initial Commit

---
+++ Github-Client-TCA/Github_Client_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Client_TCAApp.swift
+//  Github-Client-TCA
+//
+//  Created by ryunosuke.shibuya on 2022/12/16.
+//
+
+import SwiftUI
+
+@main
+struct Github_Client_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
