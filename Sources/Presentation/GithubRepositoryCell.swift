6b580e5d94d94fdcdebfb17a3bb1fc0a3ccd3af0
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2022-12-26 16:18:26 +0900

    fix: CellにReducerを保持

--- Sources/Presentation/GithubRepositoryCell.swift
+++ Sources/Presentation/GithubRepositoryCell.swift
@@ -10,59 +10,52 @@ import Domain
 import ComposableArchitecture
 
 struct GithubRepositoryCell: View {
-    let item: GithubResponse.Item
+    let store: StoreOf<CellReducer>
     
     var body: some View {
-        VStack {
-            URLImage(url: URL(string: item.avatarURL)!)
-                .frame(width: 100, height: 100)
-            
-            repositoryName
-            
-            description
-            
-            bottom
+        WithViewStore(store, observe: { $0.item }) { viewStore in
+            VStack {
+                URLImage(url: URL(string: viewStore.avatarURL)!)
+                    .frame(width: 100, height: 100)
+                
+                repositoryName(viewStore.name)
+                
+                description(viewStore.description)
+                
+                bottom(stars: viewStore.stars, language: viewStore.language)
+            }
+            .padding(5)
+            .onTapGesture {
+                viewStore.send(.onTapGesture)
+            }
+            .alert(
+                store.scope(state: \.alert),
+                dismiss: .cancelTapped
+            )
         }
-        .padding(5)
     }
     
-    var repositoryName: some View {
-        Text(item.name)
+    func repositoryName(_ name: String) -> some View {
+        Text(name)
             .font(.headline)
     }
     
-    var description: some View {
-        Text(item.description ?? "No Description")
-            .font(.caption)
+    func description(_ description: String?) -> some View {
+        Text(description ?? "No Description")
+            .font(.headline)
     }
     
-    var bottom: some View {
+    func bottom(stars: Int, language: String?) -> some View {
         HStack {
             HStack(spacing: 5) {
                 Image(systemName: "star.fill")
-                Text("\(item.stars)")
+                Text("\(stars)")
             }
             .frame(maxWidth: .infinity)
             
-            Text(item.language ?? "No Language")
+            Text(language ?? "No Language")
                 .frame(maxWidth: .infinity)
         }
         .frame(maxWidth: .infinity)
-    }
-}
-
-struct GithubRepositoryCell_Previews: PreviewProvider {
-    static var previews: some View {
-        GithubRepositoryCell(
-            item: .init(
-                id: 1,
-                avatarURL: "https://github.com/Ryu0118.png",
-                name: "Ryu0118/XCContributeRank",
-                description: "XCContributeRank is a CLI tool that allows you to check the number of lines of code, files, and comments for each person in your Xcode project.",
-                language: "Swift",
-                stars: 6
-            )
-        )
-        .previewLayout(.fixed(width: 350, height: 200))
     }
 }
